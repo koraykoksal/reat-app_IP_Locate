4a479cdd91916935b02b230be0ec1963c5f92567
koraykoksal <kksl.koray@gmail.com>
2023-07-22 00:22:55 +0300

    created all pages

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav } from 'react-bootstrap'
+import { Footer } from '../components/Footer'
+
+export const AppRouter = () => {
+  return (
+    <>
+    <Nav/>
+    
+    
+
+    <Footer/>
+    </>
+  )
+}
