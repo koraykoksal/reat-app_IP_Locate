4f1b7fc522506ccfcce6ffb4a639794a26fb18e4
koraykoksal <kksl.koray@gmail.com>
2023-07-23 17:02:57 +0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import { AppRouter } from "./router/AppRouter"
-import axios from 'axios'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { useContext } from "react"
-import { IP_Context } from './context/UserIPAddress'
 import "./css/App.css"
 
 function App() {
